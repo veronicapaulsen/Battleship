c1888952b656d78271f296033852279b8821e720
VeronicaPaulsen <veronica.paulsen@coloradocollege.edu>
2015-10-11 14:17:49 -0600

    whole grid setup and ship names are there

--- place_ships.js
+++ place_ships.js
@@ -2,11 +2,19 @@ var battleships = [];
 var placements = [];
 var letters = ['A','B','C','D','E','F','G','H','I','J'];
 var content_elem = document.getElementById( 'content' );
+var ship_list_elem = document.getElementById( 'ship_list' );
+var ship_list = ['Destroyer: 2','Cruiser: 3','Sub: 3', 'Battleship: 4','Carrier: 5'];
 var ROWS = 11;
 var COLS = 11;
 
 function pageLoaded()
 {
+    for(var i=0; i < ship_list.length; i++ )
+    {
+	var ship_name_elem = document.createElement( 'li' );
+	ship_name_elem.innerHTML = ship_list[i];
+	ship_list_elem.appendChild( ship_name_elem );
+    }
     for( var r = 0; r < ROWS; r++ )
     {
         var row_elem = document.createElement( 'tr' );
