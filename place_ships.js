c0a9489c5882a6a43ee7b1a7e2504c56f90dc8f5
VeronicaPaulsen <veronica.paulsen@coloradocollege.edu>
2015-10-11 14:01:51 -0600

    Whole initial grid set up with col letters and row numbers

--- place_ships.js
+++ place_ships.js
@@ -1,8 +1,9 @@
 var battleships = [];
 var placements = [];
+var letters = ['A','B','C','D','E','F','G','H','I','J'];
 var content_elem = document.getElementById( 'content' );
-var ROWS = 10;
-var COLS = 10;
+var ROWS = 11;
+var COLS = 11;
 
 function pageLoaded()
 {
@@ -12,10 +13,27 @@ function pageLoaded()
         for( var c = 0; c < COLS; c++ )
         {
             var cell_elem = document.createElement( 'td' );
-            var img_elem = document.createElement( 'img' );
-            img_elem.src = "ocean.jpg";
-            img_elem.style.width = "50px";
-            cell_elem.appendChild( img_elem );
+	    if( c == 0 && r != 0 )
+	    {
+		cell_elem.innerHTML = r;
+		cell_elem.style.width = "50px";
+	    }
+	    else if ( c ==0 && r == 0 )
+	    {
+		cell_elem.innerHTML = '';
+		cell_elem.style.width = "50px";
+	    }
+	    else if (r == 0)
+	    {
+		cell_elem.innerHTML = letters[c-1];
+		cell_elem.style.height = "37px";
+	    }
+	    else{
+		var img_elem = document.createElement( 'img' );
+		img_elem.src = "ocean.jpg";
+		img_elem.style.width = "50px";
+		cell_elem.appendChild( img_elem );
+	    }
             row_elem.appendChild( cell_elem );
         }
         content_elem.appendChild( row_elem );
