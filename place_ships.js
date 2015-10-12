f9e18fdea49099f8a0311fef4a3256ebe6e8ae29
VeronicaPaulsen <veronica.paulsen@coloradocollege.edu>
2015-10-12 09:59:24 -0600

    User able to add 1 ship to the board

--- place_ships.js
+++ place_ships.js
@@ -4,16 +4,20 @@ var letters = ['A','B','C','D','E','F','G','H','I','J'];
 var content_elem = document.getElementById( 'content' );
 var ship_list_elem = document.getElementById( 'ship_list' );
 var ship_list = ['Destroyer: 2','Cruiser: 3','Sub: 3', 'Battleship: 4','Carrier: 5'];
+var ship_sizes = [2, 3, 3, 4, 5];
 var ROWS = 11;
 var COLS = 11;
 
 function pageLoaded()
 {
+    content_elem.startPointRow = null;
+    content_elem.startPointCol = null;
     for(var i=0; i < ship_list.length; i++ )
     {
 	var ship_name_elem = document.createElement( 'li' );
 	ship_name_elem.innerHTML = ship_list[i];
-	ship_name_elem.onclick = addShip;
+	ship_name_elem.ship_size = ship_sizes[i];
+	ship_name_elem.onclick = clickShipType;
 	ship_list_elem.appendChild( ship_name_elem );
     }
     for( var r = 0; r < ROWS; r++ )
@@ -39,10 +43,14 @@ function pageLoaded()
 	    }
 	    else{
 		var img_elem = document.createElement( 'img' );
-		img_elem.src = "ocean.jpg";
+		img_elem.src = "Images/ocean.jpg";
 		img_elem.style.width = "50px";
+		img_elem.style.height = "37px";
+		img_elem.row = r;
+		img_elem.col = c;
 		cell_elem.appendChild( img_elem );
 		cell_elem.hasBattleship = false;
+		cell_elem.onclick =  addShip;
 	    }
             row_elem.appendChild( cell_elem );
         }
@@ -50,11 +58,10 @@ function pageLoaded()
     }
 }
 
-function addShip(evt)
+function clickShipType(evt)
 {
     var childNodes = ship_list_elem.childNodes;
-    //console.log(childNodes.length);
-    for( var i=1; i < ship_list.length; i++)
+    for( var i=1; i < childNodes.length; i++)
     {
 	if(childNodes[i].className == "highlighted")
 	{
@@ -63,3 +70,90 @@ function addShip(evt)
     }
     evt.target.className = "highlighted";
 }
+
+function isLegal(startRow, startCol, endRow, endCol)
+{
+    var distance = 0;
+    if( startRow == endRow )
+    {
+	//ship is going horizontal
+	distance = Math.abs(startCol - endCol) + 1;
+    }
+    else if( startCol == endCol )
+    {
+	//ship is going vertical
+	distance = Math.abs(startRow - endRow) + 1; 
+    }
+    else{
+	//find a way to print this error on page
+	console.log("diagonals are not allowed!");
+    }
+    return distance;
+}
+
+function placeShip(startRow, startCol, endRow, endCol)
+{
+    var row_elems = content_elem.childNodes;
+    console.log("sR: "+ startRow + " sC: " + startCol + " eR: " + endRow + " eC: " + endCol);
+    
+    if( startRow == endRow)
+    {
+	var row = row_elems[startRow+1];
+	for( var j = startCol; j <= endCol; j++)
+	{
+	    var cell_elem = row.childNodes[j];
+	    cell_elem.hasBattleship = true;
+	    var img_elem = cell_elem.childNodes[0];
+	    console.log("img_elem: " + img_elem);
+	    img_elem.src = "Images/grey.jpg";
+	}
+    }
+    else if ( startCol == endCol)
+    {
+	for( var i = startRow+1; i <= endRow+1; i++)
+	{
+	    var row = row_elems[i];
+
+	    var cell_elem = row.childNodes[startCol];
+	    cell_elem.hasBattleship = true;
+	    var img_elem = cell_elem.childNodes[0];
+	    console.log("img_elem: " + img_elem);
+	    img_elem.src = "Images/grey.jpg";
+	}
+    }
+}
+
+function addShip(evt)
+{
+    //first we have to check which kind of ship they are adding from the list
+    var childNodes = ship_list_elem.childNodes;
+    var ship_size_needed = 0;
+    var start_row = content_elem.startPointRow;
+    var start_col = content_elem.startPointCol;
+    //console.log("startrow: " + start_row);
+    //console.log("startcol: " + start_col);
+    for( var i=1; i < childNodes.length; i++)
+    {
+	if(childNodes[i].className == "highlighted")
+	{
+	    ship_size_needed = childNodes[i].ship_size;
+	}
+    }
+    //check if we are on the first click
+    if(content_elem.startPointRow == null && content_elem.startPointCol == null )
+    {
+	content_elem.startPointRow = evt.target.row;
+	content_elem.startPointCol = evt.target.col;
+    }else
+    {
+	//we're on the second click:
+	console.log("evt row: " + evt.target.row + " evt col: " + evt.target.col);
+	var size_given = isLegal(start_row, start_col, evt.target.row, evt.target.col);
+	if(size_given == ship_size_needed)
+	{
+	    placeShip(start_row, start_col, evt.target.row, evt.target.col);
+	}
+    }
+    //delete the ship type from the list
+}
+
